ff3b4b31b52bda914f0cc85bac975b0d0f39f393
Setsuna Yukiii <siang-meng@live.com>
2022-11-18 11:27:46 +0800

    add skill

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Skill = require("./models/skill");
 require("dotenv").config();
+
 const app = express();
 
 database_uri = process.env.DATABASE_URI;
+app.use(express.json());
 
 // ==================connect to mongoose==================
 
@@ -19,6 +22,46 @@ async function connect() {
 connect();
 app.get("/", (req, res) => {
     res.json("OK");
+});
+
+// =====================skills=====================
+app.get("/skills", (req, res) => {
+    Skill.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.post("/skills", (req, res) => {
+    // check if skill exist
+    Skill.findOne({ name: req.body.name }).then((result) => {
+        console.log("RESULT", result);
+        if (!result) {
+            const skill = new Skill({
+                name: req.body.name,
+                year_learnt: req.body.year_learnt,
+            });
+            skill
+                .save()
+                .then((result) => {
+                    res.send({
+                        success: true,
+                        message: "Skill added!",
+                        data:result
+                    });
+                })
+                .catch((err) => res.send(err));
+        }
+        else{
+            res.send({
+                success: false,
+                message: "Skill already exists!",
+            });
+        }
+    });
 });
 
 app.listen(8000);
